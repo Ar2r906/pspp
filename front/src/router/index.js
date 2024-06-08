0853494865066fcfe7d7fbf60144607896c09044
Artur_Ibragimov <103675840+Ar2r906@users.noreply.github.com>
2024-06-08 15:37:58 +0500

    compiller project

---
+++ front/src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+
+  },
+  {
+    path: '/about',
+    name: 'about',    
+  }
+]
+
+const router = createRouter({
+  
+})
+
+export default router
